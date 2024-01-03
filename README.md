060b5942f29ca99d3c328dc007ac9c87d904d7ff
SonuChaudhary <siddhantc982@gmail.com>
2024-01-03 17:09:52 +0530

    User authentication with sessionId and cookies

---
+++ README.md
@@ -0,0 +1,12 @@
+# Authentication_Node_Sessions_Cookies
+This is the refactored and cleaned code from the Understanding Auth in Node using Sessions and Cookies
+
+In this project I tried to keep things simple and try to hammer in the concepts behind using Sessions
+and Cookies for Authentication.
+
+To use this code do the following:
+1. Clone this repo or download it.
+2. Navigate into the location you downloaded folder in your cmd/bash/terminal
+3. Run npm install to install all the depends in this projects (express, express-session, mongoose, ejs, bcryptjs, connect-mongodb-session)
+4. Check inside of the config folder under the default.json for the mongodb URI and insert your own connection string 
+
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const config = require("config");
+
+const appController = require("./controllers/appController");
+const isAuth = require("./middleware/is-auth");
+const connectDB = require("./config/db");
+const mongoURI = config.get("mongoURI");
+
+const app = express();
+connectDB();
+
+const store = new MongoDBStore({
+  uri: mongoURI,
+  collection: "mySessions",
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+//=================== Routes ===================//
+// Login Page
+app.get("/login", appController.login_get);
+app.post("/login", appController.login_post);
+
+// Register Page
+app.get("/register", appController.register_get);
+app.post("/register", appController.register_post);
+
+app.listen(5000, console.log("App Running on http://localhost:5000"));
